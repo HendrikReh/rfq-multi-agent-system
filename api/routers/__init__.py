63e2b702d0b7cf26cc47a00568733a6b0c1465f6
Hendrik Reh <hendrik.reh@gmail.com>
2025-06-22 15:23:19 +0200

    clean-up

--- api/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-API routers for the RFQ Multi-Agent System.
-
-This package contains all the FastAPI routers for different
-functional areas of the system.
-"""
-
-# Router imports will be added as they are implemented 
